448fe8c739beb3f55d28f4e398ef833168d500c1
Alba Ruiz de la Vega <ruizdelavegaalba@gmail.com>
2026-06-08 11:47:46 +0200

    Refactor Libro class constructors and methods

--- src/articulos/Libro.java
+++ src/articulos/Libro.java
@@ -1,23 +1,22 @@
 package articulos;
 
 public class Libro extends Articulo {
-    //Atributos específico de un libto
+    
     private String autor;
     private int numPaginas;
-    //Enumeración de género
     public enum Genero { MISTERIO, TERROR, ACCION, HISTORIA, FANTASIA}
     private Genero genero;
-    //Constructor completo
+ 
     public Libro(String titulo, int anyoLanzamiento, double precioPorDia, String autor, int numPaginas, Genero genero) {
         super(titulo, anyoLanzamiento, precioPorDia);
         this.autor = autor;
         this.numPaginas = numPaginas;
         this.genero = genero;
     }
-    //Constructor vacío
+    
     public Libro() {
     }
-    //Constructor con numero de pagina a 100 por defecto
+   
     public Libro(String titulo, int anyoLanzamiento, double precioPorDia, String autor, int numPaginas, Genero genero) {
         super(titulo, anyoLanzamiento, precioPorDia);
         this.autor = autor;
@@ -49,11 +48,11 @@ public class Libro extends Articulo {
     public void setGenero(Genero genero) {
         this.genero = genero;
     }
-    //Indica si el libro tiene más de 200 páginas
+    
     public boolean esLargo(){
         return this.numPaginas > 200;
     }
-    //Representación en texto del libro
+    
     @Override
     public String toString() {
         return "[Libro]" + this.getTitulo() + ", de" + this.autor + "(" + this.getPrecioPorDia() + " €/)";
