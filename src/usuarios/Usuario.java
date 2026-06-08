6d5af3fb9cdb05b5eb4a13f0974bc54f56d5772a
Alba Ruiz de la Vega <ruizdelavegaalba@gmail.com>
2026-06-08 11:44:00 +0200

    Refactor Usuario class comments and methods

--- src/usuarios/Usuario.java
+++ src/usuarios/Usuario.java
@@ -1,22 +1,22 @@
 package usuarios;
 
 public class Usuario {
-    //Datos básicos del usuario
+
     private String nombreUsuario;
     private String contrasenya;
     private int id;
     private int anyoNacimiento;
-    //Contructor completo
+    
     public Usuario(String nombreUsuario, String contrasenya, int id, int anyoNacimiento) {
         this.nombreUsuario = nombreUsuario;
         this.contrasenya = contrasenya;
         this.id = id;
         this.anyoNacimiento = anyoNacimiento;
     }
-    //Contructor vacío
+    
     public Usuario() {
     }
-    //Getters y setters
+   
     public String getNombreUsuario() {
         return nombreUsuario;
     }
@@ -48,11 +48,11 @@ public class Usuario {
     public void setAnyoNacimiento(int anyoNacimiento) {
         this.anyoNacimiento = anyoNacimiento;
     }
-    //Devuelve true si el usuario tiene más de 18 años
+    
     public boolean esAdulto() {
         return 2025 - this.anyoNacimiento > 18;
     }
-    //Comprueba si la contraseña tiene la longitud segura
+    
     public boolean contrasenyaSegura() {
         return this.contrasenya.length() > 10;
     }
