686f70f2cc08492cfb2ece8f5a2185f784f362f1
Alba Ruiz de la Vega <ruizdelavegaalba@gmail.com>
2026-06-08 11:47:05 +0200

    Refactor Pelicula class and improve formatting

--- src/articulos/Pelicula.java
+++ src/articulos/Pelicula.java
@@ -1,19 +1,19 @@
 package articulos;
 
 public class Pelicula extends Articulo {
-    //Atributos específicos de una película
+    
     private String director;
-    private int duracion; // duración total en minutos
-    //Contructor completo
+    private int duracion; 
+    
     public Pelicula(String titulo, int anyoLanzamiento, double precioPorDia, String director, int duracion) {
         super(titulo, anyoLanzamiento, precioPorDia);
         this.director = director;
         this.duracion = duracion;
     }
-    //Contructor vacío
+   
     public Pelicula() {
     }
-    //Getters y setters
+    
     public String getDirector() {
         return director;
     }
@@ -29,17 +29,17 @@ public class Pelicula extends Articulo {
     public void setDuracion(int duracion) {
         this.duracion = duracion;
     }
-    //Muestra la duración en formato horas y minutos
+    
     public void getDuracionHorasYMinutos(){
        int horas = this.duracion / 60;
        int minutos = this.duracion % 60;
         System.out.println( horas + " horas y  " + minutos + " minutos");
     }
-    //Devuelve los segundos sobrantes
+    
     public int getDuracionSegundos(){
         return this.duracion % 60;
     }
-    //Representación en texto de la película
+    
     @Override
     public String toString() {
         return "[Pelicula]" + this.getTitulo() + ", de " + this.director + "(" + this.getPrecioPorDia() + "€/día)";
