1174ac406da6001a83b3e3fcd98fe517d723fc55
Alba Ruiz de la Vega <ruizdelavegaalba@gmail.com>
2026-06-08 11:44:44 +0200

    Refactor Cliente class and update comments

--- src/usuarios/Cliente.java
+++ src/usuarios/Cliente.java
@@ -3,20 +3,19 @@ package usuarios;
 import articulos.Articulo;
 
 public class Cliente extends Usuario {
-    //Artículo que el cliente alquila
+    
     private Articulo articulo;
-    //Númmero de socios vinculados al cliente
     private int numSocios;
-    //Contructor completo
+    
     public Cliente(String nombreUsuario, String contrasenya, int id, int anyoNacimiento, Articulo articulo, int numSocios) {
         super(nombreUsuario, contrasenya, id, anyoNacimiento);
         this.articulo = articulo;
         this.numSocios = numSocios;
     }
-    //Contructor vacío
+    
     public Cliente() {
     }
-    //Getters y setters
+    
     public Articulo getArticulo() {
         return articulo;
     }
@@ -32,11 +31,11 @@ public class Cliente extends Usuario {
     public void setNumSocios(int numSocios) {
         this.numSocios = numSocios;
     }
-    //Calcula el precio utilizando el artículo asociado
+   
     public double calcularPrecio( int numArticulos, int dias){
         return this.getArticulo().calcularPrecio(numArticulos, dias);
     }
-    //Representación en texto del cliente
+    
     @Override
     public String toString() {
         return "Cliente" + this.getNombreUsuario() + "(" + this.getId() + ")";
